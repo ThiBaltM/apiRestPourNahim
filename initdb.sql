543d0b58e08b6d73db566524da4d201581e5e1a8
ThiBaltM <thibalt.meurin@gmail.com>
2024-05-02 13:58:06 +0200

    added gitingore

--- initdb.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE products {
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100), 
-    about VARCHAR(500),
-    price FLOAT
-};
-
-INSERT INTO products (name, about, price) VALUES
-    ('My first game', 'This is an awesome game', 60.0)
